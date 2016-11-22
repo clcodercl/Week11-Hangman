51fbb0a60a5dbcfd7e05e78c79ecd4bbd40ad521
Cynthia London <cynthe@sbcglobal.net>
2016-11-22 17:54:06 -0600

    more changes

--- word.js
+++ word.js
@@ -3,3 +3,32 @@
 
 //METHODS TO CHECK LETTERS GUESSED AGAINST THE RANDOM WORD SELECTED
 //This is a constructor file
+
+//this.randomWord = function (){
+//	var word = game.value[Math.floor(Math.random() * value.length)];
+
+//}
+
+var Letter = require('./letter.js');
+
+function Word(value) {
+	// The word we want our users to guess
+	this.value = value;
+	// An array of Letter objects that represent our word
+	this.letters = value
+		.split('')
+		.map(function(l) {
+			return new Letter(l);
+		});
+}
+
+// Takes Letters and calls .show on each one,
+// collects them into a new array,
+// calls .join to return a string
+Word.prototype.render = function() {
+	return this.letters
+		.map(l => l.render()) {
+			return v.show();
+		};
+		.join('');
+};
